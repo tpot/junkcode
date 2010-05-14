26b1f60c7e29eed619513191f81d2cf3141e5895
Tim Potter <tpot@hp.com>
2010-05-14 15:27:37 +1000

    Use sorted set and json encoding for log messages.

--- redis-logger/logmsg.py
+++ redis-logger/logmsg.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
 
-import sys, redis
+import sys, datetime
+import redis, simplejson
+
+severity = sys.argv[1]          # info, warning, error
+message = sys.argv[2]
+
+# Timestamp is score
+
+now = datetime.datetime.today()
+score = '%d%02d%02d%02d%02d%02d%06d' % \
+    (now.year, now.month, now.day, now.hour, now.minute, 
+     now.second, now.microsecond)
+
+# Create log entry
+
+entry = simplejson.dumps({'time': score, 'msg': message})
 
 rd = redis.Redis()
 
-rd.publish('log:%s' % sys.argv[1], sys.argv[2])
+pipe = rd.pipeline()
+
+pipe.zadd('log:%s:backlog' % severity, entry, score)
+pipe.publish('log:%s' % severity, entry)
+pipe.execute()
